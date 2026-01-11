ae73fdf2001792146c394a89ac46bb9d113f256e
Char <73309364+CharGoldenYT@users.noreply.github.com>
2026-01-11 12:36:40 -0500

    add the ability to mod the credits+move song data

--- source/ui/credits/CreditSelectorState.hx
+++ source/ui/credits/CreditSelectorState.hx
@@ -1,16 +1,30 @@
 package ui.credits;
 
+import ui.credits.ModCreditsState.CreditFile;
+import haxe.Json;
 import objects.Credit;
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
 
 class CreditSelectorState extends BaseCreditsState
 {
+    var files:Array<String> = [];
     public function new()
     {
         super();
 
         addCredit("Universe Engine Team", "ue", 0xFF8888FF, null, "See who helped with the engine!");
         addCredit("Funkin' Crew", "funkin", 0xFFFF8888, null, "The team behind the original kick-ass game!");
+
+        files.push("ue");
+        files.push("funkin");
+
+        loadCreditFiles();
     }
+
+
 
     public override function selectThing(credit:Credit) {
         super.selectThing(credit);
@@ -21,6 +35,24 @@ class CreditSelectorState extends BaseCreditsState
                 FlxG.switchState(new UECredits());
             case "Funkin' Crew":
                 FlxG.switchState(new FunkinCredits());
+            default:
+                FlxG.switchState(new ModCreditsState(files[grpCredits.index]));
         }
+    }
+
+    function loadCreditFiles()
+    {
+        #if sys
+        for (file in FileSystem.readDirectory("assets/data/credits"))
+        {
+            var fileName:String = file.replace(".json", "");
+            var rawJson = File.getContent(Paths.json('credits/$fileName'));
+
+            var json:CreditFile = cast Json.parse(rawJson);
+
+            addCredit(json.name, json.icon, FlxColor.fromString(json.color), null, json.desc);
+            files.push(fileName);
+        }
+        #end
     }
 }
---
+++ source/ui/credits/ModCreditsState.hx
@@ -0,0 +1,55 @@
+package ui.credits;
+
+import objects.Credit;
+import sys.io.File;
+import haxe.Json;
+
+typedef CreditFile = {
+    var name:String;
+    @:optional var icon:String;
+    var color:String;
+    var desc:String;
+    var credits:Array<CreditStruc>;
+}
+
+typedef CreditStruc = {
+    var name:String;
+    @:optional var icon:String;
+    @:optional var desc:String;
+    @:optional var color:String;
+    @:optional var link:String;
+}
+
+class ModCreditsState extends BaseCreditsState
+{
+    public function new(?file:String = 'credits')
+    {
+        super();
+        loadCredits(file);
+    }
+
+    function loadCredits(file:String)
+    {
+        var rawJson:String = File.getContent(Paths.json('credits/$file'));
+
+        var file:CreditFile = cast Json.parse(rawJson);
+
+        title = file.name;
+        for (credit in file.credits)
+        {
+            addCredit(credit.name, credit.icon, FlxColor.fromString(credit.color), credit.link, credit.desc);
+        }
+    }
+
+    override function exit() {
+        super.exit();
+
+        FlxG.switchState(new CreditSelectorState());
+    }
+
+    override function selectThing(credit:Credit) {
+        super.selectThing(credit);
+
+        if (grpCredits.curMember.hasLink) FlxG.openURL(grpCredits.curMember.link);
+    }
+}
