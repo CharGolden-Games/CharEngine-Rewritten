93ff64fb021f63fc8defe6c8022ac03293b73aed
Char <73309364+CharGoldenYT@users.noreply.github.com>
2026-01-14 11:13:01 -0500

    button fix?

--- source/objects/TopBar.hx
+++ source/objects/TopBar.hx
@@ -4,18 +4,16 @@ import objects.TopButton;
 import objects.TopButton.TopButtonOption;
 
 class TopBar extends FlxSpriteGroup{
-    var options:Array<TopButton> = [];
     var barSprite:FlxSprite;
     
     public function new(?options:Array<TopButtonOption>) {
-        this.options = options;
         super();
         barSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width, 25, 0xff9a3cc0);
         add(barSprite);
 
         var x:Int = 0;
-        for (button in options){
-            button = new TopButton(options.label, barSprite.x+x, 0);
+        for (option in options){
+            var button = new TopButton({label: option.label}, barSprite.x+x, 0);
             add(button);
             x+=Std.int(button.width);
         }
--- source/objects/TopButton.hx
+++ source/objects/TopButton.hx
@@ -2,18 +2,24 @@ package objects;
 
 typedef TopButtonOption = {
     var label:String;
+    @:optional var onClick:Void->Void;
 }
 
-class TopButton extends FlxSprite{
-    var onClick:Void->Void;
+class TopButton extends FlxTypedSpriteGroup<FlxSprite>{
+    var onClick:Null<Void->Void>;
     var label:FlxText;
 
-    public function new(name:TopButtonOption, x:Float, y:Float){
+    public function new(options:TopButtonOption, x:Float, y:Float){
         super(x, y);
-        label = new FlxText(x, y, 0, TopButtonOption.label);
+        label = new FlxText(x, y, 0, options.label);
+        add(label);
+
+        onClick = options.onClick;
     }
 
     public override function update(elapsed:Float){
+        super.update(elapsed);
+
         if (FlxG.mouse.justPressed){
             if (onClick != null)
                 onClick();
